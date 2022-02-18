df893d93a9982aceb391f044e6aa90ebc140dce2
danvogiavinh <99949352+danvogiavinh@users.noreply.github.com>
2022-02-18 14:02:10 +0700

    Add files via upload
    create card

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'cô Mỹ';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Chúc Mừng 20-10';
+const titleCard = 'Đôi lời em muốn nhắn với cô';
+const contentCard = 'Vậy là đã đến ngày 8/3 rồi. Em cũng chẳng biết nói gì nữa. Cô quá tuyệt vời, không ngôn từ nào có thể diễn tả được hết. Em chỉ muốn nói rằng:Chúc cô có một ngày 8/3 vui vẻ bên gia đình, chúc cô ngày càng xinh đẹp, thành công hơn nữa trong cuộc sống.Cảm ơn cô đã đồng hành cùng với em suốt thời gian qua.';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'chị.png.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
